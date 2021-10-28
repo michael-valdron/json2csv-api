81b9a357b77576ccabf7600f812891f0eb0408e8
Michael Valdron <michael.valdron@gmail.com>
2021-10-28 18:00:12 -0400

    convert returns csv content.

--- json2csv_api/views.py
+++ json2csv_api/views.py
@@ -1,4 +1,3 @@
-from flask import json
 from flask.blueprints import Blueprint
 from flask.wrappers import Response
 
@@ -6,4 +5,4 @@ bp = Blueprint('views', __name__)
 
 @bp.route('/v1/convert', methods=['POST'])
 def convert() -> Response:
-    return Response(json.dumps({}), mimetype='application/json', status=200)
+    return Response(None, mimetype='text/csv', status=200)
