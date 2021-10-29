43619816778e68dae581cf6c1f1ceaf536fd6cbe
Michael Valdron <michael.valdron@gmail.com>
2021-10-29 00:58:12 -0400

    corrected column offset issue in views testing. added response status checking.

--- json2csv_api/test_views.py
+++ json2csv_api/test_views.py
@@ -38,12 +38,14 @@ def test_convert(client: FlaskClient) -> None:
     })
     out_data = "person,view_grades,change_grades,add_grades," \
         + "delete_grades,view_classes,change_classes,add_classes," \
-        + "delete_classes\nstudent1,0,1,0,0,0,1,0,0,0\n" \
-        + "student2,0,1,0,0,0,1,0,0,0\nstudent3,0,1,0,0,0,1,0,0,0\n" \
-        + "teacher,0,1,1,1,1,1,0,0,0\n"
+        + "delete_classes\r\nstudent1,1,0,0,0,1,0,0,0\r\n" \
+        + "student2,1,0,0,0,1,0,0,0\r\nstudent3,1,0,0,0,1,0,0,0\r\n" \
+        + "teacher,1,1,1,1,1,0,0,0\r\n"
     response = client.post('/v1/convert', data=in_data, headers=headers)
     try:
+        status = response.status_code
         result = response.data.decode('utf8')
+        assert 200 == status
         assert out_data == result
     finally:
         response.close()
