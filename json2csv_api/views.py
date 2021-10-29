b65fa11d7e54b44945539500ac6da456d3709142
Michael Valdron <michael.valdron@gmail.com>
2021-10-29 00:56:19 -0400

    completed POST /v1/convert route.

--- json2csv_api/views.py
+++ json2csv_api/views.py
@@ -1,8 +1,20 @@
+from json.decoder import JSONDecodeError
+from flask import request
 from flask.blueprints import Blueprint
 from flask.wrappers import Response
+from .models import Json, Csv
+from .util import error_response
 
 bp = Blueprint('views', __name__)
 
 @bp.route('/v1/convert', methods=['POST'])
 def convert() -> Response:
-    return Response(None, mimetype='text/csv', status=200)
+    try:
+        in_data = Json.parse(request.data)
+        if in_data.validate():
+            out_data = Csv(in_data)
+            return Response(out_data.write(), mimetype='text/csv', status=200)
+        else:
+            return Response(error_response("Form of JSON is invalid for conversion."), mimetype='application/json', status=400)
+    except JSONDecodeError as e:
+        return Response(error_response(e.msg), mimetype='application/json', status=400)        
